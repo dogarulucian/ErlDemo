9cbfb74d8473087c1d3eeb974d1ddfcba2b54b18
Luc <dogarulucian@yahoo.com>
2018-10-03 09:35:25 +0000

    Add record and tuple ex to module

--- erlangtut.erl
+++ erlangtut.erl
@@ -1,5 +1,23 @@
 -module(erlangtut).
  
 -export([hello_world/0]).
+
+-export([tuple_show/0]).
+
+-export([start/0]). 
+-record(person, {name = "", id}). 
+
  
 hello_world() -> io:fwrite("hello, world\n").
+
+
+tuple_show()->
+    P = {john,24,{june,25}} ,
+    io:fwrite(" ~p~n", [P]).
+
+
+
+start() -> 
+   P = #person{name = "John",id = 1}, 
+   io:fwrite("~p~n",[P#person.id]), 
+   io:fwrite("~p~n",[P#person.name]).
