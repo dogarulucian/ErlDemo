80673903a3f42ccec469d631ec7e8cdb7d955969
Luc <dogarulucian@yahoo.com>
2018-10-04 14:09:37 +0000

    Add processes to script

--- erlangtut.erl
+++ erlangtut.erl
@@ -4,7 +4,17 @@
 
 -export([tuple_show/0]).
 
--export([start/0]). 
+-export([start/0]).
+
+-include("user.hrl").
+
+-export([display_employee/0]).
+
+
+-export([start_proc/0]).
+ 
+-export([server/1]).
+
 -record(person, {name = "", id}). 
 
  
@@ -21,3 +31,23 @@ start() ->
    P = #person{name = "John",id = 1}, 
    io:fwrite("~p~n",[P#person.id]), 
    io:fwrite("~p~n",[P#person.name]).
+
+
+display_employee() ->
+    T = #employee{name="Flo", id=12},
+    io:fwrite(" ~p~n",[T#employee.name]).   
+ 
+start_proc()->
+    Pid1=spawn(fun() -> server("Hello") end),
+    Pid2=spawn(fun()->server("Bye") end), 
+    %Pid1!{self(),hello},
+    Pid1!Pid2!
+    %Pid2!{self(),bye}
+
+server(Message) ->
+   io:fwrite("~p",[Message]).
+
+
+
+
+
