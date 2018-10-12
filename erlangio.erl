1863df9b772013eff35b6d2f1188927f02756a54
Luc <dogarulucian@yahoo.com>
2018-10-12 13:31:05 +0000

    Add read func and sys env var

--- erlangio.erl
+++ erlangio.erl
@@ -1,11 +1,22 @@
 -module(erlangio).
--export([write_to_file/0]).
+-export([write_to_file/0,read_from_file/0, read_from_file2/0, get_sys_var/0]).
 
 write_to_file()->
-   Content = "Some content",   
+   Content = "Some content\n",   
    {ok,File}=file:open("test.txt",[write]),
    file:write(File,Content).
 
 
+read_from_file()->
+   {ok,File2}=file:open("test.txt",[read]),
+   Content = file:read(File2,1024 * 1024),
+   io:fwrite("~p~n",[Content]).
 
+
+read_from_file2()->
+   Text = file:read_file("test.txt"),
+   io:fwrite("~p~n",[Text]).
+
+get_sys_var()->
+   os:getenv("PATH").
 
